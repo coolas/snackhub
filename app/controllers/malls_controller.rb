d123390c27ff118a54c5fb09587dde2bd7793d20
Russel Nichol V. Sandiko <sandiko.russel@gmail.com>
2017-02-05 15:15:33 +0800

    initial commit

--- app/controllers/malls_controller.rb
+++ app/controllers/malls_controller.rb
@@ -2,44 +2,51 @@ class MallsController < ApplicationController
 
 
   def index
-  	@malls = Mall.all
+    @malls = Mall.all
+    #respond_to do |format|
+     #format.html
   end
-
-  def new
-  	@malls = Mall.new
-  	
-  end	
 
   def show
-  	@malls = Mall.find(params[:id])
+  	@mall = Mall.find(params[:id])
   end
+
+  def new #new record
+  	@mall = Mall.new
+  end	
 
   def create
-    @malls = Mall.new(mall_params)
-    if @malls.save
-      cookies.permanent[:last_mall_id] = @mall.id
-      flash[:notice] = "Successfully created Mall."
-    end
-    respond_with(@mall)
+  	@mall = Mall.new(mall_params)
+  	if @mall.save
+  		redirect_to malls_path
+  	else
+  		render 'new'
+  	end
   end
+
+  def edit
+  	@mall = Mall.find(params[:id])
+  end  
 
   def update
-    @malls = Mall.find(params[:id])
-    if @malls.update_attributes(mall_params)
-      flash[:notice] = "Successfully updated product."
-    end
-    respond_with(@mall, :location => mall_url)
+  	@mall = Mall.find(params[:id])
+  	if @mall.update_attributes(mall_params)
+  		redirect_to malls_path
+  	else
+  		render 'new'
+  	end
   end
 
-   def destroy
-    @malls = Mall.find(params[:id])
-    @malls.destroy
-    flash[:notice] = "Successfully destroyed product."
-    respond_with(@malls, :responder => MyResponder)
+  def destroy
+  		@mall = Mall.find(params[:id])
+  		@mall.destroy
+  		redirect_to malls_path
   end
 
   private
     def mall_params    
-      params.require(:mall).permit(:name)
-    end
+      params.require(:mall).permit(:name, :address)
+    end 
+
 end
+
--- app/models/mall.rb
+++ app/models/mall.rb
@@ -2,6 +2,6 @@ class Mall < ActiveRecord::Base
   has_many :chains, through: :chains_malls
   has_many :chains_malls
 
-  validates_presence_of :name
+ validates_presence_of :name
 
 end
