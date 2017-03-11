1797a4e694a25352bfe51e3f744caaa0d76665e6
Russel Sandiko <russelsandiko@Manuels-MacBook-Pro-15-inch-MOBILE.local>
2017-03-11 19:48:40 +0800

    api for order items

---
+++ app/controllers/api/v1/order_items_controller.rb
@@ -0,0 +1,39 @@
+module Api
+  module V1
+    class OrderItemsController < ApiController
+      respond_to :json
+      
+      def index
+        respond_with OrderItem.all
+      end
+      def new
+      end
+      def show
+        @order_item = OrderItem.find(params[:id])
+        render json: @order_item, root: false
+      end
+     
+      def create
+        @order_item = OrderItem.new(order_item_params)
+        if @order_item.save
+          render json: @order_item, status: :created, root: false
+        else
+          render json: @order_item.errors, status: :unprocessable_entity
+        end
+      end
+     
+      def update
+      end
+
+      def destroy
+       
+      end
+
+      private
+
+      def order_item_params
+        params.require(:order).permit(:item_id, :order_id)
+      end
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,7 @@ Rails.application.routes.draw do
         resources :locations
         resources :menus
         resources :orders
+        resources :order_items
         resources :users
       end
     end
