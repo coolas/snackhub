d1f8561617df7486de93e26e955c6fa64690ff84
Russel Nichol V. Sandiko <sandiko.russel@gmail.com>
2017-03-14 14:52:28 +0800

    ORDER BY ON ORDERS

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
 
   def index
     if current_user.has_role? :superadmin
-      @orders = Order.all
+      @orders = Order("id DESC").all
     elsif current_user.has_role? :admin
       @orders = Order.where(chain_id: current_user.chain_id)
     end
