bd2a7c237020a422ca88d1deadf9ddee42dedf2b
Russel Nichol V. Sandiko <sandiko.russel@gmail.com>
2017-03-15 16:03:46 +0800

    dataTables pagination

--- config/application.rb
+++ config/application.rb
@@ -22,7 +22,7 @@ module Snackhub
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
-    config.assets.initialize_on_precompile = false
+    
 
     config.middleware.insert_before ActionDispatch::Static, Rack::Cors do
         allow do
